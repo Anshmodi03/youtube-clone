be619b4696c7a24733a1ba78b2188fea04c3ec7a
Anshmodi03 <modiaastha01@gmail.com>
2026-01-11 20:26:50 +0530

    Updated code

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/uploads", express.static(path.join("uploads")));
 app.get("/", (req, res) => {
-  res.send("You tube backend is working");
+  res.send("You tube backend is working properly ");
 });
 app.use(bodyParser.json());
 app.use("/user", userroutes);
--- yourtube/src/pages/index.tsx
+++ yourtube/src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex-1 p-4">
       <CategoryTabs />
-      <Suspense fallback={<div>Loading videos...</div>}>
+      <Suspense fallback={<div>Loading videos....</div>}>
         <Videogrid />
       </Suspense>
     </main>
